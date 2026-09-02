5c059c7b7f5475298e1c84535ecf095a0d30ee12
Fernando <fernando.akistapace@gmail.com>
2026-09-02 15:32:27 -0300

    fix: adjust header layout for improved responsiveness and visibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,34 +18,39 @@ export const Header = () => {
 
 	return (
 		<>
-			<header className='items-center hidden md:flex justify-between w-full fixed top-0 z-50 h-16 bg-white/60 dark:bg-black/40 backdrop-blur-md border-b border-black/5 dark:border-white/10'>
-			<div className='flex items-center container mx-auto py-2 px-4 justify-between w-full h-full'>
-				<div className='flex items-center gap-2 justify-between w-full'>
-					<h1 className='text-md font-semibold text-neutral-900 dark:text-white'>Fernando Aquistapace</h1>
-					<ul className='gap-2 hidden md:flex items-center'>
-						{sections.map(section => (
-							<li className='item-menu text-neutral-900 dark:text-white' key={section}>
-								<button type='button' onClick={() => scrollToSection(section)} className='text-sm'>
-									{t(`header.${section}`)}
-								</button>
+			<header className='items-center flex justify-between w-full fixed top-0 z-50 h-16 bg-white/60 dark:bg-black/40 backdrop-blur-md border-b border-black/5 dark:border-white/10'>
+				<div className='hidden md:flex  items-center container mx-auto py-2 px-4 justify-between w-full h-full'>
+					<div className='flex items-center gap-2 justify-between w-full'>
+						<h1 className='text-md font-semibold text-neutral-900 dark:text-white'>Fernando Aquistapace</h1>
+						<ul className='gap-2 hidden md:flex items-center'>
+							{sections.map(section => (
+								<li className='item-menu text-neutral-900 dark:text-white' key={section}>
+									<button type='button' onClick={() => scrollToSection(section)} className='text-sm'>
+										{t(`header.${section}`)}
+									</button>
+								</li>
+							))}
+							<li className='item-menu'>
+								<DownloadResumeButton
+									size='sm'
+									className='shadow-md border-0 bg-neutral-900 text-white dark:bg-white dark:text-black'
+									variant={'outline'}
+								/>
 							</li>
-						))}
-						<li className='item-menu'>
-							<DownloadResumeButton
-								size='sm'
-								className='shadow-md border-0 bg-neutral-900 text-white dark:bg-white dark:text-black'
-								variant={'outline'}
-							/>
-						</li>
-						<li>
-							<LanguageSwitcher />
-						</li>
-						<li>
-							<ModeToggle />
-						</li>
-					</ul>
+							<li>
+								<LanguageSwitcher />
+							</li>
+							<li>
+								<ModeToggle />
+							</li>
+						</ul>
+						
+					</div>
 				</div>
-			</div>
+				<div className='flex md:hidden items-center justify-end gap-2 px-4 w-full'>
+					<LanguageSwitcher />
+				</div>
+
 			</header>
 
 			<div className='md:hidden fixed bottom-6 right-6 z-50'>
